6df5be2c2472cb692483308b4713b9f14d816b13
Christophe Ayel <CAYEL@cegid.fr>
2018-04-05 13:44:58 +0200

    Initial Commit

---
+++ PronoFootExtract/PronoFootExtract/Fixtures.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PronoFootExtract
+{
+    class Link
+    {
+        public string href { get; set; }
+    }
+    class Links
+    {
+        public Link self { get; set; }
+        public Link competition { get; set; }
+    }
+    class LinksFixture
+    {
+        public Link self { get; set; }
+        public Link competition { get; set; }
+        public Link homeTeam { get; set; }
+        public Link awayTeam { get; set; }
+
+    }
+    class HalfTimeResultFixture
+    {
+        public int goalsHomeTeam { get; set; }
+        public int goalsAwayTeam { get; set; }
+    }
+    class ResultFixture
+    {
+        public int? goalsHomeTeam { get; set; }
+        public int? goalsAwayTeam { get; set; }
+        public HalfTimeResultFixture halfTime { get; set; }
+
+    }
+    class Fixture
+    {
+        public LinksFixture _links { get; set; }
+        public DateTime date { get; set; }
+        public string status { get; set; }
+        public int matchday { get; set; }
+        public string homeTeamName { get; set; }
+        public string awayTeamName { get; set; }
+        public ResultFixture result { get; set; }
+    }
+    class Fixtures
+    {
+        public Links _links { get; set; }
+        public int count { get; set; }
+        public Fixture[] fixtures { get; set; }
+    }
+}
---
+++ PronoFootExtract/PronoFootExtract/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PronoFootExtract
+{
+    class Program
+    {
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new System.Net.WebClient())
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                Console.WriteLine("Loading fixtures : " + elapsedMs + " ms");
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+        private static bool teamMatch(Fixture match, string teamUrl)
+        {
+            return ((match._links.homeTeam.href == teamUrl) || (match._links.awayTeam.href == teamUrl));
+        }
+        static void Main(string[] args)
+        {
+            var url = "http://api.football-data.org/v1/competitions/450/fixtures";
+            var fixturesL1 = _download_serialized_json_data<Fixtures>(url);
+            foreach (Fixture match in fixturesL1.fixtures)
+            {
+                if ((match.result.goalsAwayTeam.HasValue) && teamMatch(match, "http://api.football-data.org/v1/teams/523"))
+                {
+                    System.Console.WriteLine(match.homeTeamName + " - " + match.awayTeamName+ " : " + match.result.goalsHomeTeam+"-"+ match.result.goalsAwayTeam);
+                }                
+            }
+            Console.Write(fixturesL1.fixtures.Count());
+            Console.ReadKey();
+        }
+    }
+}
